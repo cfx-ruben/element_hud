c9f91e042e1e4902ba557f89a0d0509e8b5dd49f
cfx-ruben <trinitymou9@gmail.com>
2026-09-18 20:47:06 +0200

    fix(waypoint config)

--- client/waypoint.lua
+++ client/waypoint.lua
@@ -1,8 +1,8 @@
 
 local config = require('configs.config')
 
-if not config.features.waypointHud then return end
+if not config.features.waypoint then return end
 
 CreateThread(function()
-    ReplaceHudColourWithRgba(config.features.waypointHud.color.r, config.features.waypointHud.color.g, config.features.waypointHud.color.b, config.features.waypointHud.color.a, 255)
+    ReplaceHudColourWithRgba(142, config.features.waypoint.color.r, config.features.waypoint.color.g, config.features.waypoint.color.b, config.features.waypoint.color.a)
 end)
